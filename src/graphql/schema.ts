a9bc07f0c960c78ec90b1db95cf31cb07812d3b0
Ricky Almeida <ricky.almeida.dev@gmail.com>
2021-07-17 07:26:23 -0300

    refactor: 💡 move root query to schema definition

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,6 +1,28 @@
-import {GraphQLSchema} from 'graphql'
-import QueryType from './query-type'
+import {GraphQLNonNull, GraphQLObjectType, GraphQLSchema} from 'graphql'
+import {connectionArgs, connectionFromArray} from 'graphql-relay'
+import fs from 'fs/promises'
+import path from 'path'
+import PostType from './posts/type'
 
-const schema = new GraphQLSchema({query: QueryType})
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'Query',
+    description: 'The root of all fields',
+    fields: () => ({
+      posts: {
+        type: GraphQLNonNull(PostType.connectionType),
+        args: connectionArgs,
+        resolve: async (_, args) => {
+          const stringifiedJson = await fs.readFile(
+            path.resolve(__dirname, '..', '..', 'data.json'),
+            'utf-8'
+          )
+          const json = JSON.parse(stringifiedJson)
+          return connectionFromArray(json.posts, args)
+        }
+      }
+    })
+  })
+})
 
 export default schema
