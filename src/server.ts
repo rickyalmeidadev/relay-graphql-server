55e5bf7c9dedc7eb3b89ef317030581f295f62eb
Ricky Almeida <ricky.almeida.dev@gmail.com>
2021-07-17 07:22:19 -0300

    refactor: 💡 replace apollo server for express graphql

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,7 @@
-import {ApolloServer} from 'apollo-server'
-import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
+import express from 'express'
+import {graphqlHTTP} from 'express-graphql'
 import schema from './graphql/schema'
 
-const server = new ApolloServer({
-  schema,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
-})
-
-server.listen(4000, () => console.log('Server is listening on port 4000'))
+const app = express()
+app.use('/graphql', graphqlHTTP({schema, graphiql: true}))
+app.listen(4000, () => console.log('Server is listening on port 4000'))
