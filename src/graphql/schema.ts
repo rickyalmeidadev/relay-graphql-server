3e959e4ebcc284d8cfc73a5e682bc48e2bcb3393
Ricky Almeida <ricky.almeida.dev@gmail.com>
2021-07-17 07:05:02 -0300

    feat: 🎸 create schema from root query type and setup server

---
+++ src/graphql/schema.ts
@@ -0,0 +1,6 @@
+import {GraphQLSchema} from 'graphql'
+import QueryType from './query-type'
+
+const schema = new GraphQLSchema({query: QueryType})
+
+export default schema
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log('server...')
+import {ApolloServer} from 'apollo-server'
+import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
+import schema from './graphql/schema'
+
+const server = new ApolloServer({
+  schema,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
+})
+
+server.listen(4000, () => console.log('Server is listening on port 4000'))
